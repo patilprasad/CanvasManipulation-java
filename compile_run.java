38ad7d687d2a644650d46ace72b8ffb3285ddfe5
Rohit Kulkarni <rkulkar1@stevens.edu>
2016-04-15 22:48:36 -0400

    Created compile_run.java

---
+++ compile_run.java
@@ -0,0 +1,13 @@
+/*
+Author:Rohit Kulkarni
+Description: Compile the java programs downloaded by the download_assignments program and display the output
+             to the grader/teacher.
+             Simply a java program that compiles and runs another java program, and possibly other languages.
+
+*/
+
+public class compile_run
+{
+
+
+}
