6b52566acfe0cc9590741108f06f78f63961944b
vjindal50 <vjindal@stevens.edu>
2016-04-18 22:29:12 -0400

    WINDOW UPDATE 2 Graph buttons

--- Window.java
+++ Window.java
@@ -6,44 +6,81 @@ package canvas;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 
-public class Window extends JFrame implements Graph {
-	private WindowLink graphSelect;
-	public Window(){					// this method creates the window for the canvas adding 3 buttons for now for accessing graphs
+public class Window extends JFrame {
+	public Window(String applicationTitle , String chartTitle){					// this method creates the window for the canvas adding 3 buttons for now for accessing graphs
 		super("Canvas by Vaibhav");
 	setSize(800,600);
+	JFreeChart barChart = ChartFactory.createBarChart(
+	         chartTitle,
+	         "Students","Grades",
+	         createDataset(),
+	         PlotOrientation.VERTICAL,
+	         true,true,false);
+	 JFreeChart lineChart = ChartFactory.createLineChart(
+	    	  chartTitle,
+	    	  "Years","Number of Schools",
+	    	  createDataset(),
+	    	  PlotOrientation.VERTICAL,
+	    	  true,true,false);
 	Container c = getContentPane();
 	JPanel p = new JPanel();
 	p.setBackground(Color.BLUE);
-	p.setLayout(new GridLayout(3,1));
+	p.setLayout(new GridLayout(2,1));
 	
 	JButton b = new JButton("LineChart");
 	p.add(b);
 	b.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-			graphSelect.setMode(new LineChartMode());	
+			JPanel p = new ChartPanel( lineChart );
+			c.add(p, BorderLayout.EAST);
+	
 		}
 	});
 	b = new JButton("BarChart");
 	p.add(b);
 	b.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
+			JPanel p = new ChartPanel( barChart );
+			c.add(p, BorderLayout.CENTER);
 			
 		}
 	});
-	b = new JButton("PieChart");
+	/*b = new JButton("PieChart");
 	p.add(b);
 	b.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
+		
+		
 			
 		}
 	});
-	
+	*/
 	c.add(p, BorderLayout.WEST);
 	setVisible(true);
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }
+	
+	 private DefaultCategoryDataset createDataset( )
+	   {
+	      DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
+	      dataset.addValue( 15 , "schools" , "1970" );
+	      dataset.addValue( 30 , "schools" , "1980" );
+	      dataset.addValue( 60 , "schools" ,  "1990" );
+	      dataset.addValue( 120 , "schools" , "2000" );
+	      dataset.addValue( 240 , "schools" , "2010" );
+	      dataset.addValue( 300 , "schools" , "2014" );
+	      return dataset;
+	   }
 public static void main(String[] a) {
-	new Window();
+	Window chart= new Window("Grade Vs Students" ,
+		      "Grades Vs Stdents");
 	}
 }
