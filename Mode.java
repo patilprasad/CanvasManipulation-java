77864c631bd60d8d36a6947352430d869699d7fe
vjindal50 <vjindal@stevens.edu>
2016-04-18 22:41:09 -0400

    files not needed anymore

--- Mode.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author : vaibhav
- */
-package canvas;
-
-public abstract class Mode implements Graph {	// this abstract class forces all the mode classes to have a 'public Graph create' method
-	public abstract Graph create(String applicationTitle , String chartTitle);
-
-}
